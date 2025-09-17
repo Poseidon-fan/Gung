e43f4fe0d181f8d52b5c1214a5d4307155ced92f
Poseidon <18646154381@163.com>
2025-09-17 09:57:04 +0000

    feat: json data serde in auth req payload

--- components/auth/src/lib.rs
+++ components/auth/src/lib.rs
@@ -1,1 +1,12 @@
 mod msg;
+
+use pyo3::prelude::*;
+
+use crate::msg::AuthReq;
+
+pub fn register_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
+    let auth_module = PyModule::new(parent_module.py(), "auth")?;
+    auth_module.add_class::<AuthReq>()?;
+    parent_module.add_submodule(&auth_module)?;
+    Ok(())
+}
--- components/auth/src/msg.rs
+++ components/auth/src/msg.rs
@@ -1,2 +1,25 @@
-#[allow(unused)]
-pub const MSG: &str = "Hello, world!";
+use pyo3::{exceptions::PyValueError, prelude::*};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[pyclass]
+#[derive(Serialize, Deserialize)]
+pub struct AuthReq {
+    payload: Value,
+}
+
+#[pymethods]
+impl AuthReq {
+    #[new]
+    fn new(payload: Py<PyAny>, py: Python) -> PyResult<Self> {
+        let value = pythonize::depythonize(payload.bind(py))?;
+        Ok(Self { payload: value })
+    }
+
+    #[getter]
+    fn payload(&self, py: Python<'_>) -> PyResult<Py<PyAny>> {
+        pythonize::pythonize(py, &self.payload)
+            .map(|bound| bound.unbind())
+            .map_err(|e| PyErr::new::<PyValueError, _>(e.to_string()))
+    }
+}
