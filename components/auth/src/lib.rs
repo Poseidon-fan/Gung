f5548fe19cc0231866651820428d2caa34f1373b
Poseidon <18646154381@163.com>
2025-09-17 10:39:44 +0000

    feat: auth msg implment in python

--- components/auth/src/lib.rs
+++ components/auth/src/lib.rs
@@ -2,11 +2,15 @@ mod msg;
 
 use pyo3::prelude::*;
 
-use crate::msg::AuthReq;
+use crate::msg::{AuthAcceptResp, AuthChallengeResp, AuthRejectResp, AuthReq, AuthResp};
 
 pub fn register_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     let auth_module = PyModule::new(parent_module.py(), "auth")?;
     auth_module.add_class::<AuthReq>()?;
+    auth_module.add_class::<AuthResp>()?;
+    auth_module.add_class::<AuthAcceptResp>()?;
+    auth_module.add_class::<AuthRejectResp>()?;
+    auth_module.add_class::<AuthChallengeResp>()?;
     parent_module.add_submodule(&auth_module)?;
     Ok(())
 }
--- components/auth/src/msg.rs
+++ components/auth/src/msg.rs
@@ -1,25 +1,74 @@
 use pyo3::{exceptions::PyValueError, prelude::*};
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[pyclass]
-#[derive(Serialize, Deserialize)]
 pub struct AuthReq {
     payload: Value,
 }
 
+#[pyclass]
+pub enum AuthResp {
+    Accept(AuthAcceptResp),
+    Reject(AuthRejectResp),
+    Challenge(AuthChallengeResp),
+}
+
+#[pyclass]
+#[derive(Clone)]
+pub struct AuthAcceptResp {
+    #[pyo3(get)]
+    msg: String,
+}
+
+#[pyclass]
+#[derive(Clone)]
+pub struct AuthRejectResp {
+    #[pyo3(get)]
+    msg: String,
+}
+
+#[pyclass]
+#[derive(Clone)]
+pub struct AuthChallengeResp {
+    #[pyo3(get)]
+    msg: String,
+    #[pyo3(get)]
+    required_fields: Vec<String>,
+}
+
 #[pymethods]
 impl AuthReq {
-    #[new]
-    fn new(payload: Py<PyAny>, py: Python) -> PyResult<Self> {
-        let value = pythonize::depythonize(payload.bind(py))?;
-        Ok(Self { payload: value })
-    }
-
     #[getter]
     fn payload(&self, py: Python<'_>) -> PyResult<Py<PyAny>> {
         pythonize::pythonize(py, &self.payload)
             .map(|bound| bound.unbind())
             .map_err(|e| PyErr::new::<PyValueError, _>(e.to_string()))
+    }
+}
+
+#[pymethods]
+impl AuthAcceptResp {
+    #[new]
+    fn new(msg: String) -> Self {
+        Self { msg }
+    }
+}
+
+#[pymethods]
+impl AuthRejectResp {
+    #[new]
+    fn new(msg: String) -> Self {
+        Self { msg }
+    }
+}
+
+#[pymethods]
+impl AuthChallengeResp {
+    #[new]
+    fn new(msg: String, required_fields: Vec<String>) -> Self {
+        Self {
+            msg,
+            required_fields,
+        }
     }
 }
