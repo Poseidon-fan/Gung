7fa90959b99134da629077fea3925f34e453b34a
Poseidon <18646154381@163.com>
2025-09-18 03:36:12 +0000

    feat: run config

--- server/src/config/run.rs
+++ server/src/config/run.rs
@@ -2,15 +2,18 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct RunConfig {
-    pub server: ServerConfig,
+    pub transport: TransportConfig,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct ServerConfig {
-    pub quic_config: Option<QuicConfig>,
+#[serde(rename_all = "lowercase")]
+pub enum TransportConfig {
+    Quic(QuicTransportConfig),
+    Tcp(TcpTransportConfig),
 }
 
 #[derive(Debug, Deserialize)]
-pub struct QuicConfig {
-    pub port: u16,
-}
+pub struct QuicTransportConfig {}
+
+#[derive(Debug, Deserialize)]
+pub struct TcpTransportConfig {}
