3bcc11964216d3a9444a60e149f2399550a36d15
Poseidon <18646154381@163.com>
2025-10-04 05:17:30 +0000

    feat: new Transport abstraction && import net-mux

--- components/transport/src/lib.rs
+++ components/transport/src/lib.rs
@@ -1,20 +1,29 @@
 mod quic;
 
 use anyhow::Result;
-
 use async_trait::async_trait;
 use tokio::{
     io::{AsyncRead, AsyncWrite},
-    net::{ToSocketAddrs, unix::SocketAddr},
+    net::unix::SocketAddr,
 };
 
 #[async_trait]
 pub trait Transport: Send + Sync {
     type Listener: Send + Sync;
+    type RawConnection: Send + Sync + AsyncRead + AsyncWrite;
+    type Connection: LogicConnection<Stream = Self::Channel>;
+    type Channel: Send + Sync + AsyncRead + AsyncWrite;
+
+    async fn bind(addr: SocketAddr) -> Result<Self::Listener>;
+    async fn accept(l: &mut Self::Listener) -> Result<(Self::RawConnection, SocketAddr)>;
+    async fn connect(addr: SocketAddr) -> Result<Self::RawConnection>;
+    fn establish(raw_conn: Self::RawConnection, is_server: bool) -> Result<Self::Connection>;
+}
+
+#[async_trait]
+pub trait LogicConnection: Send + Sync {
     type Stream: Send + Sync + AsyncRead + AsyncWrite;
 
-    async fn bind<T: ToSocketAddrs + Send + Sync>(&self, addr: T) -> Result<Self::Listener>;
-    async fn accept(&self, l: &mut Self::Listener) -> Result<(Self::Stream, SocketAddr)>;
-    async fn connect<T: ToSocketAddrs + Send + Sync>(&self, addr: T) -> Result<Self::Stream>;
-    async fn close(&self, l: Self::Listener);
+    async fn accept() -> Result<Self::Stream>;
+    async fn open() -> Result<Self::Stream>;
 }
--- components/transport/src/quic.rs
+++ components/transport/src/quic.rs
@@ -1,22 +1,99 @@
 #![allow(dead_code, unused_variables)]
-use anyhow::Result;
+
 use async_trait::async_trait;
-use quinn::{Endpoint, Incoming, RecvStream, SendStream, rustls::Connection};
 use tokio::{
     io::{AsyncRead, AsyncWrite},
-    net::{ToSocketAddrs, unix::SocketAddr},
+    net::unix::SocketAddr,
 };
 
-use crate::Transport;
+use crate::{LogicConnection, Transport};
 
 pub struct QuicTransport {}
 
-pub struct QuicListener(Endpoint, Incoming);
+#[async_trait]
+impl Transport for QuicTransport {
+    type Listener = quinn::Endpoint;
+    type RawConnection = QuicRawConnection;
+    type Connection = QuicConnection;
+    type Channel = QuicStream;
+
+    async fn bind(addr: SocketAddr) -> anyhow::Result<Self::Listener> {
+        todo!()
+    }
+
+    async fn accept(l: &mut Self::Listener) -> anyhow::Result<(Self::RawConnection, SocketAddr)> {
+        todo!()
+    }
+
+    async fn connect(addr: SocketAddr) -> anyhow::Result<Self::RawConnection> {
+        todo!()
+    }
+
+    fn establish(
+        raw_conn: Self::RawConnection,
+        is_server: bool,
+    ) -> anyhow::Result<Self::Connection> {
+        todo!()
+    }
+}
+
+pub struct QuicConnection(quinn::Connection);
 
 pub struct QuicStream {
-    connection: Connection,
-    rx: RecvStream,
-    tx: SendStream,
+    sender: quinn::SendStream,
+    receiver: quinn::RecvStream,
+}
+
+pub struct QuicRawConnection {
+    conn: quinn::Connection,
+    stream: QuicStream,
+}
+
+impl AsyncRead for QuicRawConnection {
+    fn poll_read(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        todo!()
+    }
+}
+
+impl AsyncWrite for QuicRawConnection {
+    fn poll_write(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<Result<usize, std::io::Error>> {
+        todo!()
+    }
+
+    fn poll_flush(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        todo!()
+    }
+
+    fn poll_shutdown(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        todo!()
+    }
+}
+
+#[async_trait]
+impl LogicConnection for QuicConnection {
+    type Stream = QuicStream;
+
+    async fn accept() -> anyhow::Result<Self::Stream> {
+        todo!()
+    }
+
+    async fn open() -> anyhow::Result<Self::Stream> {
+        todo!()
+    }
 }
 
 impl AsyncRead for QuicStream {
@@ -49,50 +126,6 @@ impl AsyncWrite for QuicStream {
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), std::io::Error>> {
-        todo!()
-    }
-
-    fn poll_write_vectored(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-        bufs: &[std::io::IoSlice<'_>],
-    ) -> std::task::Poll<Result<usize, std::io::Error>> {
-        let buf = bufs
-            .iter()
-            .find(|b| !b.is_empty())
-            .map_or(&[][..], |b| &**b);
-        self.poll_write(cx, buf)
-    }
-
-    fn is_write_vectored(&self) -> bool {
-        false
-    }
-}
-
-impl Drop for QuicStream {
-    fn drop(&mut self) {
-        todo!()
-    }
-}
-
-#[async_trait]
-impl Transport for QuicTransport {
-    type Listener = QuicListener;
-    type Stream = QuicStream;
-
-    async fn bind<T: ToSocketAddrs + Send + Sync>(&self, addr: T) -> Result<Self::Listener> {
-        todo!()
-    }
-
-    async fn accept(&self, l: &mut Self::Listener) -> Result<(Self::Stream, SocketAddr)> {
-        todo!()
-    }
-
-    async fn connect<T: ToSocketAddrs + Send + Sync>(&self, addr: T) -> Result<Self::Stream> {
-        todo!()
-    }
-
-    async fn close(&self, l: Self::Listener) {
         todo!()
     }
 }
