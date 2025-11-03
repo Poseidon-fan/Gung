fc3a89bc7d7a563d3aa697bf4cbc97aafac7f428
Poseidon <18646154381@163.com>
2025-11-03 06:09:29 +0000

    feat: add tracing log support

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,8 +2,18 @@ use clap::Parser;
 use config::client::CliConfig;
 
 use anyhow::Result;
+use tracing_subscriber::EnvFilter;
 
 fn main() -> Result<()> {
     let cli_config = CliConfig::parse();
+
+    const DEFAULT_LEVEL: &str = "info";
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::from(DEFAULT_LEVEL)),
+        )
+        .with_ansi(atty::is(atty::Stream::Stdout))
+        .init();
+
     client::run_client(cli_config)
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,9 +3,19 @@ use std::fs;
 use anyhow::Result;
 use clap::Parser;
 use config::server::{CliConfig, Command, RunConfig};
+use tracing_subscriber::EnvFilter;
 
 fn main() -> Result<()> {
     let cli_config = CliConfig::parse();
+
+    const DEFAULT_LEVEL: &str = "info";
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::from(DEFAULT_LEVEL)),
+        )
+        .with_ansi(atty::is(atty::Stream::Stdout))
+        .init();
+
     match cli_config.command {
         Command::Run { config_file_path } => {
             let run_config: RunConfig = toml::from_str(&fs::read_to_string(config_file_path)?)?;
