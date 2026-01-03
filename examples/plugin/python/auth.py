9de4f55ec7fa04fa301f93c35f565c27ae09b265
Poseidon <18646154381@163.com>
2026-01-03 11:09:12 +0000

    fix: string auth in example

--- examples/plugin/python/auth.py
+++ examples/plugin/python/auth.py
@@ -4,7 +4,7 @@ def authenticate(ctx: auth.AuthContext) -> auth.AuthResp:
     print(ctx.requests[-1].payload)
     if ctx.requests[-1].payload.get("password") == None:
         return auth.AuthResp.challenge("password is required", ["password"])
-    elif ctx.requests[-1].payload.get("password") != 123456:
+    elif ctx.requests[-1].payload.get("password") != "123456":
         return auth.AuthResp.reject("password is incorrect")
     else:
         return auth.AuthResp.accept("pass")
